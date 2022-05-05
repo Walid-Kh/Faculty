ee0da6d7faf8b95fb67b3fd551d373844c2b906f
Walid-Kh <walid.osama.khamees@gmail.com>
2022-05-06 01:59:55 +0200

    ""

--- Year1/2ndTerm/externalProblems/recursion/reversestring.cpp
+++ Year1/2ndTerm/externalProblems/recursion/reversestring.cpp
@@ -4,26 +4,26 @@ using namespace std;
 class solution
 {
 public:
-  void reverseString (vector<char>& s)
+  void reverseString(vector<char> &s)
   {
-    reverseString(&s[0], s.size()); 
+    reverseString(&s[0], s.size());
   }
-  void reverseString (char* s, int size)
+  void reverseString(char *s, int size)
   {
     if (size <= 1)
-      return; 
-      char temp = s[0]; 
-      s[0] = s[size-1]; 
-      s[size-1] = temp; 
-      reverseString(s+1,size-2); 
+      return;
+    char temp = s[0];
+    s[0] = s[size - 1];
+    s[size - 1] = temp;
+    reverseString(s + 1, size - 2);
   }
 };
 int main()
 {
-  vector<char> x = {'H','a','n','n','a','h'};
+  vector<char> x = {'H', 'a', 'n', 'n', 'a', 'h'};
   solution s;
-  s.reverseString(x);  
+  s.reverseString(x);
   for (auto i : x)
-    cout << i; 
+    cout << i;
   return 0;
 }
